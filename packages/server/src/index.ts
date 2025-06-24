4638219b22a3a356e514d7203738ab5e452ec3cd
Shibashis Deb <shibashis.deb@technoidentity.com>
2025-06-24 18:41:21 +0530

    fix cors error

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -18,6 +18,20 @@ app.use(cors({
   optionsSuccessStatus: 204
 }));
 
+// Additional CORS headers as direct Express middleware
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+  
+  // Handle preflight
+  if (req.method === 'OPTIONS') {
+    res.sendStatus(200);
+  } else {
+    next();
+  }
+});
+
 // Regular middleware
 app.use(express.json());
 
